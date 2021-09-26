3c83711ee1e2d866d28be76fa250f5da1f5d91c2
Purnimavrm25 <78846144+Purnimavrm25@users.noreply.github.com>
2021-09-26 23:20:24 +0530

    size.cpp
    this program tells us the size of int, float, double and char

---
+++ size.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+void main()
+{
+int int;
+char char;
+double double;
+float float;
+cout<<"Size of int:"<<sizeof(int)<<" byte"<<endl;
+cout<<"Size of float:"<<sizeof(float)<<" bytes"<<endl;
+cout<<"Size of double:"<<sizeof(double)<<" bytes"<<endl;
+cout<<"Size of char:"<<sizeof(char)<<" bytse"<<endl;
+}
